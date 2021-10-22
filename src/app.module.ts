ac9156aa8085f17fa0efa93ea4a449951ea6a8ab
Luke Brown <me@luke.sx>
2021-10-22 10:12:58 +0100

    Learning Pipes, DTOs and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CatsModule } from './cats/cats.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [UserModule, CatsModule, MongooseModule.forRoot('mongodb+srv://hub-user:fZZaCbgM5wkhZhWY@hub-staging.hppcn.mongodb.net/iqos-hub?retryWrites=true&w=majority')],
+  imports: [UserModule, CatsModule, MongooseModule.forRoot('mongodb+srv://hub-user:fZZaCbgM5wkhZhWY@hub-staging.hppcn.mongodb.net/iqos-hub?retryWrites=true&w=majority'), MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Cat, CatSchema } from './schemas/cat.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+  imports: [],
   controllers: [CatsController],
   providers: [CatsService],
 })
