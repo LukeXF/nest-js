87e0c19c081b9c58a6a4c5d5abf00d201c24fc9a
Luke Brown <me@luke.sx>
2021-10-23 19:44:48 +0100

    Dependency Injection system

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { UserModule } from './user/user.module';
 import { CatsModule } from './cats/cats.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
+import { CpuModule } from './cpu/cpu.module';
+import { DiskModule } from './disk/disk.module';
+import { PowerModule } from './power/power.module';
 
 @Module({
-  imports: [UserModule, CatsModule, MongooseModule.forRoot('mongodb+srv://hub-user:fZZaCbgM5wkhZhWY@hub-staging.hppcn.mongodb.net/iqos-hub?retryWrites=true&w=majority'), MessagesModule],
+  imports: [UserModule, CatsModule, MongooseModule.forRoot('mongodb+srv://hub-user:fZZaCbgM5wkhZhWY@hub-staging.hppcn.mongodb.net/iqos-hub?retryWrites=true&w=majority'), MessagesModule, ComputerModule, CpuModule, DiskModule, PowerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -3,19 +3,17 @@ import {MessagesRepository} from "./messages.repository";
 
 @Injectable()
 export class MessagesService {
-    messagesRepo: MessagesRepository;
-
-    constructor() {
-        // don't do this
-        this.messagesRepo = new MessagesRepository();
+    constructor(public messagesRepo: MessagesRepository) {
     }
 
     findOne(id: string) {
         return this.messagesRepo.findOne(id)
     }
+
     findAll() {
         return this.messagesRepo.findAll()
     }
+
     create(content: string) {
         return this.messagesRepo.create(content)
     }
