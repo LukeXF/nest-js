97b45777718c1fdd83d739f2ee95938802988162
Luke Brown <me@luke.sx>
2021-10-24 21:37:16 +0100

    db.sqlite module and user/report entities + users/report system

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,41 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { CatsModule } from './cats/cats.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MessagesModule } from './messages/messages.module';
-import { ComputerModule } from './computer/computer.module';
-import { CpuModule } from './cpu/cpu.module';
-import { DiskModule } from './disk/disk.module';
-import { PowerModule } from './power/power.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
+import {CatsModule} from './cats/cats.module';
+import {MongooseModule} from '@nestjs/mongoose';
+import {MessagesModule} from './messages/messages.module';
+import {ComputerModule} from './computer/computer.module';
+import {CpuModule} from './cpu/cpu.module';
+import {DiskModule} from './disk/disk.module';
+import {PowerModule} from './power/power.module';
+import {UsersModule} from './users/users.module';
+import {ReportsModule} from './reports/reports.module';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {User} from './user/entities/user.entity';
+import {Report} from './reports/report.entity';
 
 @Module({
-  imports: [UserModule, CatsModule, MongooseModule.forRoot('mongodb+srv://hub-user:fZZaCbgM5wkhZhWY@hub-staging.hppcn.mongodb.net/iqos-hub?retryWrites=true&w=majority'), MessagesModule, ComputerModule, CpuModule, DiskModule, PowerModule],
-  controllers: [AppController],
-  providers: [AppService],
+    // MongooseModule.forRoot('mongodb+srv://hub-user:fZZaCbgM5wkhZhWY@hub-staging.hppcn.mongodb.net/iqos-hub?retryWrites=true&w=majority')
+    imports: [
+        UserModule,
+        CatsModule,
+        TypeOrmModule.forRoot({
+            type: 'sqlite',
+            database: 'db.sqlite',
+            entities: [User, Report],
+            synchronize: true,
+        }),
+        MessagesModule,
+        ComputerModule,
+        CpuModule,
+        DiskModule,
+        PowerModule,
+        UsersModule,
+        ReportsModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
-
+export class AppModule {
+}
